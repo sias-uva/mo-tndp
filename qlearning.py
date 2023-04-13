6b3103ba085c9daac423c860888e0d376ee92f23
dimichai <dimitris@hey.com>
2023-04-13 18:04:20 +0200

    implemented qlearning algorithm

---
+++ qlearning.py
@@ -0,0 +1,129 @@
+from pathlib import Path
+import random
+
+import numpy as np
+from motndp.city import City
+import gymnasium
+from gymnasium.envs.registration import register
+import matplotlib.pyplot as plt
+
+register(
+    id="motndp_dilemma-v0",
+    entry_point="motndp.motndp:MOTNDP"
+)
+
+alpha = 0.7 # learning rate
+gamma = 1
+epsilon = 1
+max_epsilon = 1
+min_epsilon = 0.01
+decay = 0.0001
+
+train_episodes = 10000
+test_episodes = 1
+nr_stations = 8
+seed = 42
+
+# follow pre-determined policy
+policy = None
+# whether to mask out actions that lead to cells that are already selected
+mask_selected_cells = False
+# Best policy
+# policy = [0, 0, 0, 0, 2, 2, 2, 2]
+
+if __name__ == '__main__':
+    dilemma_dir = Path(f"./cities/dilemma_5x5")
+    city = City(
+            dilemma_dir, 
+            groups_file="groups.txt",
+            ignore_existing_lines=True
+    )
+    
+    env = gymnasium.make('motndp_dilemma-v0', city=city, nr_stations=nr_stations)
+
+    Q = np.zeros((env.observation_space.n, env.action_space.n))
+    rewards = []
+    epsilons = []
+    training_step = 0
+
+    best_episode_reward = 0
+    best_episode_segment = []
+    for episode in range(train_episodes):
+        state, info = env.reset(seed=seed, loc=(4, 0))
+        episode_reward = 0
+        episode_step = 0
+        while True:
+            #Choosing an action given the states based on a random number
+            exp_exp_tradeoff = random.uniform(0, 1) 
+            
+            state_index = city.grid_to_vector(state['location'][None, :]).item()
+
+            if policy:
+                action = policy[episode_step]
+            # exploit
+            elif exp_exp_tradeoff > epsilon:
+                action = np.argmax(Q[state_index, :])
+            # explore
+            else:
+                action = env.action_space.sample(mask=info['action_mask'])
+            
+            new_state, reward, done, _, info = env.step(action)
+
+            # Here we sum the reward to create a single-objective policy optimization
+            reward = reward.sum()
+
+            # Update Q-Table
+            new_state_gid = city.grid_to_vector(new_state['location'][None, :]).item()
+            Q[state_index, action] = Q[state_index, action] + alpha * (reward + gamma * np.max(Q[new_state_gid, :]) - Q[state_index, action])
+            episode_reward += reward      
+            state = new_state    
+
+            training_step += 1
+            episode_step += 1
+            print(f'step {training_step}, episode_step: {episode_step}, state: {state}, action: {action}, reward: {reward} new_state: {new_state}')
+
+            if done:
+                break
+        #Cutting down on exploration by reducing the epsilon 
+        epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay * episode)
+
+        if episode_reward > best_episode_reward:
+            best_episode_reward = episode_reward
+            best_episode_segment = info['segments']
+        
+        # Adding the total reward and reduced epsilon values
+        rewards.append(episode_reward)
+        epsilons.append(epsilon)
+
+    print('all good')
+    #Visualizing results and total reward over all episodes
+    x = range(train_episodes)
+    plt.plot(x, rewards, label='rewards')
+    plt.plot(x, epsilons, label='epsilons')
+    plt.xlabel('Episode')
+    plt.ylabel('Training total reward')
+    plt.ylim(0, None)
+    plt.title('Total rewards over all episodes in training') 
+    plt.legend()
+    plt.show()
+
+    # Testing the agent
+    total_rewards = 0
+    for episode in range(test_episodes):
+        state, info = env.reset(seed=seed, loc=(4, 0))
+        episode_reward = 0
+        locations = []
+        while True:
+            state_index = city.grid_to_vector(state['location'][None, :]).item()
+            locations.append(state['location'])
+            action = np.argmax(Q[state_index,:])      
+            new_state, reward, done, _, _ = env.step(action)
+            reward = reward.sum()
+            episode_reward += reward      
+            state = new_state    
+            if done:
+                break
+        total_rewards += episode_reward
+
+    print('Line Segments: ', locations)
+
