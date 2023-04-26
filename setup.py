cbca7724a5cc6cb38c645ad7a8c83bf4cf888d57
dimichai <dimichai@outlook.com>
2023-04-26 15:55:58 +0200

    setup.py added description and other config

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="motndp",
+    description="Multi-Objective Transport Network Design Problem using the city of Amsterdam",
     version="0.0.1",
-    install_requires=["gymnasium==0.27.1", "pygame==2.1.0"],
+    install_requires=["gymnasium==0.27.1"],
+    packages=find_packages(),
+    include_package_data=True,
 )
